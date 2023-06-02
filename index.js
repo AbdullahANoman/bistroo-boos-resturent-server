2e7a7b6a93fda55ad7aad3921f8fa9c100f11a5a
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-06-02 20:30:15 +0600

    added delete user and add specific user by filterign with email and add the users

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -24,28 +24,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const usersCollection = client.db("bistrooDB").collection("users");
     const menuCollection = client.db("bistrooDB").collection("menu");
     const cartCollection = client.db("bistrooDB").collection("carts");
 
+    //users api collection
+    app.get('/users', async(req,res)=>{
+      const result  = await usersCollection.find().toArray()
+      res.send(result)
+    })
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        res.send([]);
+      } else {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+    //menu collection for api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
     // cart collection
-    app.get('/carts', async(req,res)=>{
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      console.log(email)
-      const query = { email:email };
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
       const results = await cartCollection.find(query).toArray();
-      res.send(results)
-    })
+      res.send(results);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
